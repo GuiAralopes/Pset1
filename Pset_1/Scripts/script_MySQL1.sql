b905325f31dde72cb532a6b661739306c9ca063b
6ui4r4uj0 <62915262+GuiAralopes@users.noreply.github.com>
2022-04-26 14:08:14 -0300

    Update script_MySQL1.sql

--- Pset_1/Scripts/script_MySQL1.sql
+++ Pset_1/Scripts/script_MySQL1.sql
@@ -1,7 +1,7 @@
 CREATE USER 'guiaraujo'@'localhost' IDENTIFIED BY 'pgdb123';
 
 -- Criar o banco de dados e o esquema no MariaDB
-CREATE SCHEMA uvv
+CREATE database uvv
 --codificação UTF-8
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
