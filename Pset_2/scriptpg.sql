ebb89ee9352eaf36cf7a89634019d1e2135311f9
6ui4r4uj0 <62915262+GuiAralopes@users.noreply.github.com>
2022-05-09 18:35:06 -0300

    Update scriptpg.sql

--- Pset_2/scriptpg.sql
+++ Pset_2/scriptpg.sql
@@ -1,18 +1,18 @@
 -- QUESTÃO 01: prepare um relatório que mostre a média salarial dos funcionários de cada departamento.
 SELECT  numero_departamento, 
-       AVG(salario) AS media_salarial 	-- usa a função average para calcular a media
-FROM funcionario 						-- seleciona a tabela funcionario 
+       AVG(salario) AS media_salarial 		-- usa a função average para calcular a media
+FROM funcionario 							-- seleciona a tabela funcionario 
 GROUP BY numero_departamento
-order BY numero_departamento ;			-- agrupado e ordenado pelo número de departamento 
+order BY numero_departamento ;				-- agrupado e ordenado pelo número de departamento 
 
 
 --QUESTÃO 02: prepare um relatório que mostre a média salarial dos homens e das mulheres.
 SELECT sexo, 
-       AVG(salario) AS media_genero		--usa a função average para calcular a média
+       AVG(salario) AS media_genero			--usa a função average para calcular a média
        
-FROM funcionario						-- seleciona a tabela funcionario  			
+FROM funcionario							-- seleciona a tabela funcionario  			
 GROUP BY sexo
-ORDER BY sexo ;							--agrupado e ordenado  por sexo
+ORDER BY sexo ;								--agrupado e ordenado  por sexo
 
 
 /*QUESTÃO 03: prepare um relatório que liste o nome dos departamentos e, para
@@ -20,8 +20,8 @@ cada departamento, inclua as seguintes informações de seus funcionários: o no
 completo, a data de nascimento, a idade em anos completos e o salário.*/
 
 SELECT
-	f.primeiro_nome ||' '|| f.nome_meio||' '||f.ultimo_nome as nome_completo, --selecionado os nomes de todos os funcionários e agrupamos por nome_completo 
-	f.data_nascimento, 
+	f.primeiro_nome ||' '|| f.nome_meio||' '||f.ultimo_nome as nome_completo, 		--selecionado os nomes de todos os funcionários e agrupamos por nome_completo 
+	f.data_nascimento, 																--selecionado a data de nascimento 
 	DATE_PART('year', current_date)- DATE_PART('year', f.data_nascimento) as idade, --idade do funcionário através da data atual menos a data_nascimento
 	f.salario,
 	d.nome_departamento 
@@ -29,7 +29,7 @@ FROM
 	funcionario f 
 
 JOIN departamento d on f.numero_departamento  = d.numero_departamento 
-ORDER BY nome_departamento 			--ordenado por nome_departamento 
+ORDER BY nome_departamento 															--ordenado por nome_departamento 
 
 
 /*QUESTÃO 04:prepare um relatório que mostre o nome completo dos funcionários, a idade em anos completos, o salário atual e o salário com um reajuste que
@@ -52,3 +52,5 @@ FROM
 do gerente e o nome dos funcionários. Ordene esse relatório por nome do departamento 
 (em ordem crescente) e por salário dos funcionários (em ordem decrescente).
 */
+
+		
